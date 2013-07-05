ed557b5f2829e47740ae0afa1e68e9233c199827
Joao Trindade <trindade.joao@gmail.com>
2013-07-05 10:28:16 +0100

    Started doing homework 2

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+    var buffer = fs.readFileSync('index.html')
+    response.send("Buffer" + buffer);
+    response.send('Hello World2!here');
 });
 
 var port = process.env.PORT || 5000;
